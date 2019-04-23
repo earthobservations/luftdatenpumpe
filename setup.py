789ff696b1ea8cf619a7d3d2e620efc75f67890d
Andreas Motl <andreas@hiveeyes.org>
2019-04-23 22:23:25 +0200

    Bump library versions

--- setup.py
+++ setup.py
@@ -8,32 +8,32 @@ README = open(os.path.join(here, 'README.rst')).read()
 requires = [
 
     # Core
-    'six==1.11.0',
+    'six==1.12.0',
     'appdirs==1.4.3',
     'docopt==0.6.2',
     'munch==2.3.2',
-    'tqdm==4.28.1',
+    'tqdm==4.31.1',
 
     # Acquisition
-    'requests==2.20.1',
-    'tablib==0.12.1',
+    'requests==2.21.0',
+    'tablib==0.13.0',
 
     # Caching
-    'requests-cache==0.4.13',
-    'dogpile.cache==0.6.8',
-    'redis==3.0.1',
+    'requests-cache==0.5.0',
+    'dogpile.cache==0.7.1',
+    'redis==3.2.1',
 
     # Adapters
     'paho-mqtt==1.4.0',
     'dataset==1.1.2',
-    'psycopg2-binary==2.7.6.1',
-    'GeoAlchemy2==0.5.0',
-    'influxdb==5.2.1',
+    'psycopg2-binary==2.8.2',
+    'GeoAlchemy2==0.6.1',
+    'influxdb==5.2.2',
     'SQLAlchemy-Utils==0.33.11',
 
     # Geospatial
     'geohash2==1.1',
-    'geopy==1.18.0',
+    'geopy==1.19.0',
 
     # Grafana
     'Jinja2==2.10.1',
